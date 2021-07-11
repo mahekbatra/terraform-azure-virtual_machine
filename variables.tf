d88fbd3411df65fc22b417b9d8b8a43093d6c32d
Mahek Batra <mahekbatra7@gmail.com>
2021-07-11 15:28:52 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group"{ 
+   
+}
+
+variable "location"{
+  
+}
+
+
+variable "subscriptionId"{
+
+}
+
+variable "prefix"{
+  
+}
+
+variable "computer_name"{
+  
+}
+
+
+variable "admin_username"{
+   
+}
+
+variable "admin_password"{
+   
+}
